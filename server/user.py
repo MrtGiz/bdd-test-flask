f62195c5e1df4c8d4ebdcd58b7e7017526a40e59
Marat Gizzatullin <maratgiz@gmail.com>
2020-06-14 14:54:38 +0300

    connection fix

--- server/user.py
+++ server/user.py
@@ -1,56 +1,81 @@
 import sqlite3
+from sqlite3 import Error
 from flask import jsonify
 from flask_restx import Namespace, Resource
+
+DB_FILE = 'db.sqlite'
 
 api = Namespace('users', description='User resources')
 
 parser = api.parser()
 parser.add_argument("name", type=str, required=True, help="The user name")
 
-#---------------------------------------------------------------------------
+
 @api.route('/users/<int:user_id>')
 @api.param('user_id', description='User ID')
 class User(Resource):
+
     def get(self, user_id=None):
         """ Возвращает пользователя с запрошенным id в формате Json """
-        db = sqlite3.connect('db.sqlite')
+        db = create_connection(DB_FILE)
         c = db.cursor()
         if user_id:
             c.execute("select name from appuser where id = ?", (user_id,))
-            for name, in c:
+
+            try:
+                name = c.fetchone()[0]
                 return jsonify(id=user_id, name=name)
-            return "User not found", 404
+            except Exception as err:
+                print(err)
+                return "User not found", 404
+            finally:
+                db.close()
 
 
 @api.route('/users/')
 class Users(Resource):
+
     def get(self):
         """ Возвращает список пользователей в формате Json """
         users = list()
-        db = sqlite3.connect('db.sqlite')
+        db = create_connection(DB_FILE)
         c = db.cursor()
         c.execute("select * from appuser")
 
         for user_id, name in c:
             users.append(dict(id=user_id, name=name))
+        db.close()
         return jsonify(users)
 
     @api.doc(parser=parser)
     def post(self):
-        """ Добавление нового пользователя в БД """
+        """ Добавляет нового пользователя в БД """
         args = parser.parse_args()
-        db = sqlite3.connect('db.sqlite')
+        db = create_connection(DB_FILE)
         c = db.cursor()
-        user_id = self.max_id() + 1
-        c.execute("INSERT INTO appuser VALUES (?, ?)", (user_id, args['name']))
+
+        new_user_id = self.max_id(db) + 1
+        c.execute("INSERT INTO appuser VALUES (?, ?)", (new_user_id, args['name']))
         db.commit()
-        return user_id, 201
+        db.close()
+        return new_user_id, 201
 
     @staticmethod
-    def max_id():
+    def max_id(conn):
         """ Нахождение максимального текущего id пользователя """
-        db = sqlite3.connect('db.sqlite')
-        c = db.cursor()
+        c = conn.cursor()
         c.execute("select max(id) from appuser")
-        mid = c.fetchone()[0]
-        return mid
+        max_id = c.fetchone()[0]
+        return max_id
+
+
+def create_connection(db_file):
+    """ создает соединение с базой данных SQLite, указанной в db_file """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+        return conn
